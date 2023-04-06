3ae9d38d641fed7cc894655c49333ca620c8c1c1
zergro <erling_95@hotmail.com>
2023-04-06 17:45:24 +0300

    Fix?

--- next.config.js
+++ next.config.js
@@ -1,26 +1,6 @@
-const isProduction = process.env.NODE_ENV === 'production';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-module.exports = {
-  async headers() {
-    return [
-      {
-        // Define the origin of your frontend app, e.g. http://localhost:8080
-        source: isProduction ? 'https://junior-project-frontend.vercel.app/:path*' : '/',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: isProduction ? 'https://erlingjuniorproductsite.000webhostapp.com/products.php' : 'http://localhost:3000/products.php',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'Origin, X-Requested-With, Content-Type, Accept',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
-          },
-        ],
-      },
-    ];
-  },
-};
+module.exports = nextConfig
